bae78fe4ec52065379397ff02b8697a05cb5bd89
lucky962 <leoterray@yahoo.com.au>
2019-01-06 13:05:05 +1100

    Changed %-I to %I

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ async def on_message(message):
                 if str(row[0]) == trainingid:
                     print('FOUND ONE')
                     trainingtype = row[1]
-                    time = str(datetime.datetime.strptime(str(row[2])[11:16], '%H:%M').strftime('%-I:%M %p'))
+                    time = str(datetime.datetime.strptime(str(row[2])[11:16], '%H:%M').strftime('%I:%M %p'))
                     date = str(datetime.datetime.strptime(str(row[2])[:10], '%Y-%m-%d').strftime('%d %B %Y'))
                     formattedtime = str(row[2])
                     host = row[3]
@@ -282,8 +282,8 @@ async def on_reaction_add(reaction,user):
             if 'pm' in formattedtime.lower() or 'am' in formattedtime.lower():
                 formattedtime = time = formattedtime.replace(' ','')
                 formattedtime = date + ' ' + formattedtime
-                TrainingTime = datetime.datetime.strptime(formattedtime, '%d/%m/%Y %-I:%M%p')
-                time = str(datetime.datetime.strptime(str(time), '%-I:%M%p').strftime('%-I:%M %p'))
+                TrainingTime = datetime.datetime.strptime(formattedtime, '%d/%m/%Y %I:%M%p')
+                time = str(datetime.datetime.strptime(str(time), '%I:%M%p').strftime('%I:%M %p'))
             elif not ':' in formattedtime:
                 if len(formattedtime) == 4:
                     formattedtime = time = formattedtime[:2] + ':' + formattedtime[2:]
@@ -294,7 +294,7 @@ async def on_reaction_add(reaction,user):
             elif ':' in formattedtime:
                 formattedtime = date + ' ' + formattedtime
                 TrainingTime = datetime.datetime.strptime(formattedtime, '%d/%m/%Y %H:%M')
-                time = str(datetime.datetime.strptime(str(time), '%H:%M').strftime('%-I:%M %p'))
+                time = str(datetime.datetime.strptime(str(time), '%H:%M').strftime('%I:%M %p'))
             else:
                 await client.send_message(reaction.message.channel, 'Time Format not recognised')
             currenttime = str(datetime.datetime.now() - datetime.timedelta(hours=11))
