60f2834c30363d7c865bf842e660ccc9e66a4698
Leo <leoterray@yahoo.com.au>
2019-01-26 14:42:11 +1100

    Updated help msg

--- bot.py
+++ bot.py
@@ -592,7 +592,7 @@ async def on_message(message):
                 HelpMsg.add_field(name=(CMDPrefix.get(message.guild.id)) + 'trainingreminder [id]', value='**SD+ Only** - Sends a training reminder about the specified training.')
                 HelpMsg.add_field(name=(CMDPrefix.get(message.guild.id)) + 'edittraining [id] [fieldtochange]: [valuetochangeto]', value='**SD+ Only** - edits training session specified **Currently in BETA**')
                 HelpMsg.add_field(name=(CMDPrefix.get(message.guild.id)) + 'canceltraining [id]', value='**SD+ Only** - deletes training session specified.')
-                HelpMsg.add_field(name=(CMDPrefix.get(message.guild.id)) + 'mytrainings', value='**SD+ Only** - Displays your hosted training sessions.')
+                HelpMsg.add_field(name=(CMDPrefix.get(message.guild.id)) + 'mytrainings [user(optional)]', value='**SD+ Only** - Displays user\'s or your hosted training sessions.')
                 HelpMsg.add_field(name=(CMDPrefix.get(message.guild.id)) + 'alltrainings', value='Displays all training sessions.')
                 HelpMsg.set_footer(icon_url=client.user.avatar_url, text='© Alton County Railways')
                 await message.channel.send(embed=HelpMsg)
