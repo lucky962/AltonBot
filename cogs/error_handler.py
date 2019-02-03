23dca95d05673a5c421e3144088b7ecf08e4484a
Leo <leoterray@yahoo.com.au>
2019-02-03 22:32:16 +1100

    Added sending error message

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -30,6 +30,7 @@ class CommandErrorHandler:
             
         print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        await ctx.send((traceback.format_exc().replace('leote','username')).split('\n')[-2])
     
     # @commands.command(name='repeat', aliases=['mimic', 'copy'])
     # async def do_repeat(self, ctx, *, inp: str):
